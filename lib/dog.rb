0eb30814f3f17a50bd54075a78d403ed815e121f
Kenneth Valdivia <kenneth_valdivia@yahoo.com>
2020-01-07 19:54:30 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,5 @@
+class Dog 
+  def bark
+    puts "woof!"
+    def sit 
+      puts "The Dog is sitting"
---
+++ lib/person.rb
@@ -0,0 +1,5 @@
+class Person
+  def talk
+    puts "Hello World!"
+    def walk
+      puts "The Person is walking"
